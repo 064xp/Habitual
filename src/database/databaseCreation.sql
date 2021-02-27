d6e5b01dd65c02502bf1a1bb077f5864f74d70a3
Paulo <paulozamora2000@gmail.com>
2021-02-26 18:14:06 -0600

    Script de creación de base de datos. Tablas iniciales.

---
+++ src/database/databaseCreation.sql
@@ -0,0 +1,41 @@
+-- Database: habitual
+
+-- DROP DATABASE habitual;
+
+CREATE DATABASE habitual
+    WITH 
+    OWNER = paulo
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE TABLE Users (
+	userID SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE HabitTypes (
+	typeID INTEGER PRIMARY KEY NOT NULL,
+	name VARCHAR(50) UNIQUE NOT NULL,
+	days INTEGER UNIQUE NOT NULL
+);
+
+CREATE TABLE Habits (
+	habitID SERIAL PRIMARY KEY NOT NULL,
+	userID INTEGER REFERENCES Users (userID) NOT NULL,
+	name VARCHAR(200) NOT NULL,
+	frequency INTEGER NOT NULL,
+	type INTEGER REFERENCES HabitTypes (typeID) NOT NULL,
+	startDate DATE NOT NULL,
+	daysPending INTEGER NOT NULL
+);
+
+CREATE TABLE History (
+	entryID SERIAL PRIMARY KEY NOT NULL,
+	habitID INTEGER REFERENCES Habits (habitID) NOT NULL,
+	dateTime TIMESTAMP NOT NULL
+);
