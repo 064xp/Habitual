804d05a8d46c7d0516d27efae344320040ea6bc4
Paulo <paulozamora2000@gmail.com>
2021-03-01 23:00:38 -0600

    Cambios de estilo. Trigger para actualizar dias pendientes cuando hay update al habito.

--- src/database/databaseCreation.sql
+++ src/database/databaseCreation.sql
@@ -4,7 +4,7 @@
 
 CREATE DATABASE habitual
     WITH 
-    OWNER = paulo
+    OWNER = habitual
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
@@ -42,57 +42,96 @@ CREATE TABLE History (
 
 -- Stored Procedures
 CREATE OR REPLACE PROCEDURE insertHabit(
-	name VARCHAR(200),
-	userID INTEGER,
-	frequency INTEGER,
-	type INTEGER,
-	startDate DATE = CURRENT_DATE
+	_name VARCHAR(200),
+	_userID INTEGER,
+	_frequency INTEGER,
+	_type INTEGER,
+	_startDate DATE = CURRENT_DATE
 )
 LANGUAGE plpgsql
 AS $$
-	DECLARE typeDays INTEGER;
-	BEGIN
-		SELECT days INTO typeDays FROM HabitTypes WHERE typeID = type;
-		IF NOT FOUND THEN
-			RAISE EXCEPTION 'Invalid habit type %', type;
-			RETURN;
-		END IF;
-		INSERT INTO Habits (userID, name, frequency, type, startDate, daysPending)
-			VALUES (userID, name, frequency, type, startDate, typeDays);
-		RAISE NOTICE 'success';
-	END
+DECLARE 
+	typeDays INTEGER;
+BEGIN
+	SELECT days INTO typeDays FROM HabitTypes WHERE typeID = type;
+	IF NOT FOUND THEN
+		RAISE EXCEPTION 'Invalid habit type %', type;
+		RETURN;
+	END IF;
+	INSERT INTO Habits (userID, name, frequency, type, startDate, daysPending)
+		VALUES (userID, name, frequency, type, startDate, typeDays);
+	RAISE NOTICE 'success';
+END
 $$;
 
 CREATE OR REPLACE PROCEDURE insertUser(
-	name VARCHAR(100),
-	email VARCHAR(100),
-	password VARCHAR(50)
+	_name VARCHAR(100),
+	_email VARCHAR(100),
+	_password VARCHAR(50)
 )
 LANGUAGE plpgsql
 AS $$
-	BEGIN
-		INSERT INTO Users (name, email, password)
-			VALUES (name, email, password);
-		EXCEPTION
-			WHEN unique_violation THEN
-				RAISE EXCEPTION 'Email is already in use';
-	END
+BEGIN
+	INSERT INTO Users (name, email, password)
+		VALUES (name, email, password);
+	EXCEPTION
+		WHEN unique_violation THEN
+			RAISE EXCEPTION 'Email is already in use';
+END
 $$;
 
--- Functions
+CREATE OR REPLACE PROCEDURE updateHabit(
+	_habitID INTEGER,
+	_name VARCHAR(200),
+	_frequency INTEGER,
+	_type INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	UPDATE Habits SET name=name, frequency=_frequency, type=_type
+		WHERE habitID = _habitID;
+END
+$$;
 
-CREATE OR REPLACE FUNCTION daysPending (_habitID INTEGER)
+-- Trigger
+CREATE TRIGGER tr_updateDaysPending
+AFTER UPDATE ON Habits
+FOR EACH ROW
+	EXECUTE PROCEDURE updateDaysPending();
+
+-- Functions
+CREATE OR REPLACE FUNCTION getDaysPending (_habitID INTEGER, _newType INTEGER)
 RETURNS INTEGER
 LANGUAGE plpgsql
 AS $$
-	DECLARE
-		habitType INTEGER := (SELECT type FROM Habits WHERE habitID = _habitID);
-		totalDays INTEGER := (SELECT days FROM HabitTypes WHERE typeId = habitType);
-		activitiesDone INTEGER := (SELECT COUNT(DISTINCT dateTime::date) FROM History WHERE habitID = _habitID);	
-		
-	BEGIN
-		RETURN totalDays - activitiesDone;
-	END
+DECLARE
+	totalDays INTEGER := (SELECT days FROM HabitTypes WHERE typeId = _newType);
+	activitiesDone INTEGER := (SELECT COUNT(DISTINCT dateTime::date) FROM History WHERE habitID = _habitID);	
+	daysPending INTEGER;
+BEGIN
+	SELECT totalDays - activitiesDone INTO daysPending;
+	IF daysPending < 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN daysPending;
+	END IF;
+END
+$$;
+
+-- Trigger Functions
+CREATE OR REPLACE FUNCTION updateDaysPending()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	IF OLD.type <> NEW.type THEN
+		UPDATE Habits SET daysPending=getDaysPending(NEW.habitID, NEW.type)
+					WHERE habitID = NEW.habitID;
+	END IF;
+
+	RETURN NEW;
+END
 $$;
 
 -- Insert initial data 
