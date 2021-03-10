e3e30af7f592b0a77db873faa5ddf51bbd4b14a0
Paulo <paulozamora2000@gmail.com>
2021-03-09 20:23:43 -0600

    El password se guarda encriptado en vez de en texto plano.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const { insertUser } = require("../db.js");
 const { validateUser } = require("../validation.js");
 
 router.post("/sign-up", async (req, res) => {
+  let newUser = { ...req.body };
+  // Validate data
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Hashing the password
+  const salt = await bcrypt.genSalt(10);
+  newUser.password = await bcrypt.hash(req.body.password, salt);
+
+  // Insert into DB
   try {
-    const userId = await insertUser(req.body);
+    const userId = await insertUser(newUser);
     return res.json({ userId: userId });
   } catch (e) {
     //email already in use
