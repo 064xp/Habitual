c36e59a6155fc8689d1cd6db667a143217844c2b
Paulo <paulozamora2000@gmail.com>
2021-04-04 23:46:01 -0500

    UI para escoger días de recordatorio. Fix a función toUTCTime.

--- src/public/assets/js/habitForm.js
+++ src/public/assets/js/habitForm.js
@@ -7,13 +7,14 @@ window.onload = function () {
 
 function toggleRecordatorio(e) {
   var timeInput = document.querySelector("#input-recordatorio");
+  var recContainer = document.querySelector(".recordatorio-container");
   if (e.target.checked) {
     if (timeInput.value.length === 0) {
       timeInput.value = "00:00";
     }
-    timeInput.style.display = "block";
+    recContainer.style.display = "block";
   } else {
-    timeInput.style.display = "none";
+    recContainer.style.display = "none";
   }
 }
 
@@ -23,15 +24,23 @@ function onSubmit(e) {
   var recordatorio = form.recordar.checked
     ? toUTCTime(form.recordatorio.value)
     : null;
+  var dias = [];
+
+  Object.values(form.diasRec).forEach(function (dia) {
+    if (dia.checked) dias.push(dia.value);
+  });
 
   var data = {
     name: form.nombre.value,
     type: form.tipo.value,
     frequency: form.frecuencia.value,
     reminder: recordatorio,
+    reminderDays: dias,
   };
 
-  if ((site = "nuevoHabito")) {
+  console.log(data);
+
+  if (site == "nuevoHabito") {
     //enviar a ruta de nuevo habito
   } else if (site == "editarHabito") {
     //enviar a ruta de editar habito
@@ -41,8 +50,11 @@ function onSubmit(e) {
 function toUTCTime(timeStr) {
   //Offset en horas a tiempo UTC
   var horasMin = timeStr.split(":");
-  var offset = new Date().getTimezoneOffset() / 60;
-  var utcHours = (parseInt(horasMin[0]) + offset) % 24;
+  var offset = new Date().getTimezoneOffset();
+  var totalMin = parseInt(horasMin[0]) * 60 + parseInt(horasMin[1]);
+  var horaUTC = Math.floor(((totalMin + offset) / 60) % 24);
+  var horaStr = horaUTC.toString();
+  horaStr = horaStr.length == 1 ? "0" + horaStr : horaStr;
 
-  return utcHours.toString() + ":" + horasMin[1];
+  return horaStr + ":" + horasMin[1];
 }
